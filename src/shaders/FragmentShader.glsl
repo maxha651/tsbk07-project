a89dcff428daa8626ce1588f0f83e1549fcdb5ef
Friduric <frepr183@student.liu.se>
2015-05-12 21:37:47 +0200

    Added a simple lightning model. Signed-off-by: Friduric <frepr183@student.liu.se>

--- src/shaders/FragmentShader.glsl
+++ src/shaders/FragmentShader.glsl
@@ -2,9 +2,16 @@
 
 out  vec4 out_Color;
 
+in vec3 ex_Normal;
+
 uniform vec4 uni_Color;
 
 void main(void)
 {
-  out_Color = vec4(uni_Color);
+	const vec3 light = vec3(0.58, 0.58, 0.58);
+	float shade;
+
+	shade = dot(normalize(ex_Normal), light);
+
+	out_Color = vec4(uni_Color.x * shade, uni_Color.y * shade, uni_Color.z * shade, 1.0);
 }
--- src/shaders/VertexShader.glsl
+++ src/shaders/VertexShader.glsl
@@ -1,13 +1,14 @@
 #version 330 core
  
 in vec3 in_Position;
+in vec3 in_Normal;
 
-
+out vec3 ex_Normal;
 
 uniform mat4 transform;
 
 void main(void)
 {
    gl_Position = transform * vec4(in_Position, 1.0);
-  
+   ex_Normal = in_Normal;
 }
