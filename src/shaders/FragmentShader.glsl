08b94d852d584d470cc879d5b06e8d20b6aca57c
Friduric <frepr183@student.liu.se>
2015-05-12 21:41:02 +0200

    Even better lightning model! Signed-off-by: Friduric <frepr183@student.liu.se>

--- src/shaders/FragmentShader.glsl
+++ src/shaders/FragmentShader.glsl
@@ -11,7 +11,7 @@ void main(void)
 	const vec3 light = vec3(0.58, 0.58, 0.58);
 	float shade;
 
-	shade = dot(normalize(ex_Normal), light);
+	shade = clamp(dot(normalize(ex_Normal), light), 0.0, 1.0);
 
 	out_Color = vec4(uni_Color.x * shade, uni_Color.y * shade, uni_Color.z * shade, 1.0);
 }
