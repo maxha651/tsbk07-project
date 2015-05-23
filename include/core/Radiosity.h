0693f1adaa51eda7c2b7700e833607d19136d1ee
davli538 <davli538@student.liu.se>
2015-05-23 16:26:46 +0200

    added a lamp and started on radiosity class
    Signed-off-by: davli538 <davli538@student.liu.se>

---
+++ include/core/Radiosity.h
@@ -0,0 +1,43 @@
+#pragma once 
+
+//
+// Created by David on 2015-05-23.
+//
+
+// C++ std lib
+#include <cstdlib>
+#include <vector>
+#include <cstdio>
+
+// Third party
+#include <Eigen/Geometry>
+
+// Ours
+#include <Color.h>
+#include <Patch.h>
+#include <RaycastMesh.h>
+
+using Eigen::Vector3f;
+
+/**
+* @Class to calculate radiosity for given modells/patches
+*/
+class Radiosity {
+
+public:
+	
+	Radiosity();
+	virtual ~Radiosity();
+	void AddPatches(std::vector<Patch> p);
+	void CalculateRadiosity();
+	void CreateRayCastMesh();
+
+	RaycastMesh *rm;
+	std::vector<Patch> patches;
+	std::vector<int> indices;
+	std::vector<float> vertices;
+
+private:
+
+};
+
