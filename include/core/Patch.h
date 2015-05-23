a69987ef9338df4b71142d965ad93389fadf6b26
Friduric <frepr183@student.liu.se>
2015-05-23 13:47:29 +0200

    Patch class. cpp + h. Signed-off-by: Friduric <frepr183@student.liu.se>

---
+++ include/core/Patch.h
@@ -0,0 +1,19 @@
+#pragma once 
+
+//
+// Created by Fredrik on 2015-05-23.
+//
+
+/**
+ * @brief Class for a patch used with the radiosity algorithm.
+ */
+class Patch {
+
+public:
+	Patch();
+	virtual ~Patch();
+
+private:
+   
+};
+
---
+++ src/core/Patch.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Fredrik on 2015-05-23.
+//
+
+#include <Patch.h>
+
+Patch::Patch() {
+
+}
+
+Patch::~Patch() {
+    
+}
