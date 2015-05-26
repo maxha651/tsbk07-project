5e303414f09f8bb50f2042f2ab31df37c64a3861
Friduric <frepr183@student.liu.se>
2015-05-26 13:45:43 +0200

    Documentation and comments in radiosity.cpp. Signed-off-by: Friduric <frepr183@student.liu.se>

--- src/core/Radiosity.cpp
+++ src/core/Radiosity.cpp
@@ -1,5 +1,5 @@
 //
-// Created by David on 2015-05-23.
+// Created by David and Fredrik on 2015-05-23.
 //
 
 #include <Radiosity.h>
@@ -18,9 +18,10 @@ Radiosity::~Radiosity() {
 
 }
 
+// Calculates energies and radiosity for all patches.
 void Radiosity::CalculateRadiosity(){
-	// LineRenderer
 	
+	// LineRenderer for debugging
 	GameObject &go = (Context::Instance().game)->GetGameObject("linerenderer");
 	LineRenderer *l = go.GetComponent<LineRenderer>();
 
@@ -63,9 +64,12 @@ void Radiosity::CalculateRadiosity(){
 
 					float v = currentPatch->normal.dot(targetedPatch->normal) / (currentPatch->normal.norm()*targetedPatch->normal.norm());
 
+					// Do not share energy with patches that have the same "rotation" or "angle" as the current patch.
 					if (v < 0.999){
 
+						// Wether we should use raycast as visibility detection.
 						if (usingRaycast){
+							
 							// Cast ray.
 							RmReal hitLocation[3] = { 0, 0, 0 }; // We probably don't need to initialize this.
 							RmReal normal[3]; // We do not use this. The library we use point the normal in wrong direction. Instead fetch normal from target patch.
@@ -88,24 +92,22 @@ void Radiosity::CalculateRadiosity(){
 							l->AddLine(f, nn);
 							l->AddLine(Vector3f(from[0], from[1], from[2]), Vector3f(to[0], to[1], to[2]));
 						}*/
-	
-						
-
 					}
 				}
 			}
 		}
 
-		// Calculate the radiosity.
-
+		// Calculate the radiosity and energy bouncing.
 		for (int k = 0; k < iterations; k++){
 			std::cout << "Iteration: " << k << std::endl;
-			// Add emitted energy to total energy
+
+			// Add emitted energy to total energy.
 			for (int i = 0; i < patches.size(); i++){
 				Vector3f E = patches[i]->emittedEnergy;
 				patches[i]->totalEnergy += E / (float)iterations;
 			}
 			
+			// Iterate over all patches for every patch.
 			for (int i = 0; i < patches.size(); i++){
 				Patch* currentPatch = patches[i].get();
 
@@ -124,6 +126,7 @@ void Radiosity::CalculateRadiosity(){
 						(targetedPatch->vert1.y() + targetedPatch->vert2.y() + targetedPatch->vert3.y()) / 3.0f,
 						(targetedPatch->vert1.z() + targetedPatch->vert2.z() + targetedPatch->vert3.z()) / 3.0f); 
 
+					// Calculate distance to hit from patch.
 					float hitDistance = (from - to).norm();
 
 					// Calculate angles from normal to patches.
@@ -136,16 +139,15 @@ void Radiosity::CalculateRadiosity(){
 					// Calculate the total energy to a temp variable so that we can do multiple iterations with bouncing.
 					// Using the radiosity equation described at wikipedia: http://en.wikipedia.org/wiki/Radiosity_(computer_graphics)
 					float x = targetedPatch->reflectivity * currentPatch->color.x() * targetedPatch->totalEnergy.x() * (1.0f / (M_PI*hitDistance*hitDistance)) * phi1 * phi2 * targetedPatch->area / (float)iterations;
-					float y = targetedPatch->reflectivity * currentPatch->color.y() * targetedPatch->totalEnergy.y()* (1.0f / (M_PI*hitDistance*hitDistance)) * phi1 * phi2 * targetedPatch->area / (float)iterations;
+					float y = targetedPatch->reflectivity * currentPatch->color.y() * targetedPatch->totalEnergy.y() * (1.0f / (M_PI*hitDistance*hitDistance)) * phi1 * phi2 * targetedPatch->area / (float)iterations;
 					float z = targetedPatch->reflectivity * currentPatch->color.z() * targetedPatch->totalEnergy.z() * (1.0f / (M_PI*hitDistance*hitDistance)) * phi1 * phi2 * targetedPatch->area / (float)iterations;
 					Vector3f energy = Vector3f(x, y, z);
 					currentPatch->totalEnergyTemp += energy;
 				}
-				// Finalize current patch total energy temp with the emitted energy and total from earlier iterations.
 
+				// Finalize current patch total energy temp with the emitted energy and total energy from earlier iterations.
 				currentPatch->totalEnergyTemp = currentPatch->totalEnergy + currentPatch->totalEnergyTemp;
 			}
-
 
 			// For each iteration we need to zero the temp energy and set the actual total energy to the total temp energy.
 			for (int i = 0; i < patches.size(); i++){
@@ -156,11 +158,13 @@ void Radiosity::CalculateRadiosity(){
 }
 
 
-
+// Adds patches that we then can use when calculating radiosity.
 void Radiosity::AddPatches(std::vector<std::shared_ptr<Patch>> p){
 	patches.insert(patches.end(), p.begin(), p.end());
 }
 
+// Creates a raycast mesh from the patches that have been added. 
+// The raycast mesh can then be used for the visibility detection when calculating radiosity energy.
 void Radiosity::CreateRayCastMesh(){
 	if (!usingRaycast) {
 		return;
